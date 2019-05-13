f70b42eaa7262284d97ec6194f85cd536b303f95
sathishcst10 <sathishcst10@gmail.com>
2019-05-13 18:24:30 +0530

    1 file modified

--- Hello.js
+++ Hello.js
@@ -1,3 +1,15 @@
 import React from 'react';
 
 export default ({ name }) => <h1>Hello {name}!</h1>;
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCSUF_j2L5rGY_Vc9Lx4hCQ8V8a6HXDD04",
+//   authDomain: "simplelogin-sathid.firebaseapp.com",
+//   databaseURL: "https://simplelogin-sathid.firebaseio.com",
+//   projectId: "simplelogin-sathid",
+//   storageBucket: "simplelogin-sathid.appspot.com",
+//   messagingSenderId: "155265472801",
+//   appId: "1:155265472801:web:016b3ebba45188c2"
+// };
